5c007574dbc9a23f696dfef6fe424dfdeed29497
Jan Schröder <34372068+vier31@users.noreply.github.com>
2023-12-19 04:41:11 +0000

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'vier31https://.github.io',
+  base: 'altes-haus-mit-garten',
   image: {
       domains: [
           'astro.build'
